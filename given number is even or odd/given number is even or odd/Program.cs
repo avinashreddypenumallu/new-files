7d7eadfde2b203d4e3ab39c2a0dcfa4459df886f
avinashreddypenumallu <30487440+avinashreddypenumallu@users.noreply.github.com>
2017-08-08 23:12:26 -0500

    First commit

---
+++ given number is even or odd/given number is even or odd/Program.cs
@@ -0,0 +1,27 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace given_number_is_even_or_odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number");
+            int i = Convert.ToInt32(Console.ReadLine());
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("the given number is even");
+                Console.ReadLine();
+            }
+  else
+            { 
+          Console.WriteLine("the given number is odd");
+                Console.ReadLine();
+  }
+        }
+    }
+}
