504cae7ce633fd58327da859252358c276c522d1
avinashreddypenumallu <30487440+avinashreddypenumallu@users.noreply.github.com>
2017-08-08 22:59:20 -0500

    First commit

---
+++ array/array/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+
+            numbers[0]= 4;
+            numbers[1] = 8;
+            numbers[2] = 15;
+            numbers[3] = 16;
+            numbers[4] = 23;
+            //Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers.Length);
+            Console.ReadLine();
+        }
+    }
+}
